c3a27ce834d3cacf06889075cff2f3caffc4b3cb
Daniel Johansson <johansson.gd@gmail.com>
2020-10-10 16:16:56 +0200

    Klar med challenge 1

---
+++ 1. Foreach challange/ForeachChallange/ForeachChallange/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForeachChallange.Models
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
---
+++ 1. Foreach challange/ForeachChallange/ForeachChallange/Program.cs
@@ -0,0 +1,49 @@
+﻿using ForeachChallange.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ForeachChallange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Challange 1 //
+
+            //var StringList = new List<string>
+            //{
+            //    "Anna",
+            //    "Bella",
+            //    "Carl",
+            //    "David"
+            //};
+
+            //foreach (var item in StringList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            // Bonus challange //
+
+            var PersonList = new List<Person>
+            {
+                new Person
+                {
+                    Firstname = "John",
+                    Lastname = "Smith"
+                },
+                new Person
+                {
+                    Firstname = "Jane",
+                    Lastname = "Adams"
+                }
+            };
+
+            foreach (var person in PersonList)
+            {
+                Console.WriteLine($"{person.Firstname} {person.Lastname}");
+            }
+        }
+    }
+}
