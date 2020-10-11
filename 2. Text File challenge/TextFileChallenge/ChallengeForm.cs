153e5e6dcad8f3e33f9e4ac97d6e75efe7373bf6
Daniel Johansson <johansson.gd@gmail.com>
2020-10-11 17:47:54 +0200

    klar med TextFile del 1

---
+++ 2. Text File challenge/TextFileChallenge/ChallengeForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextFileChallenge
+{
+    public partial class ChallengeForm : Form
+    {
+        string path = @"C:\Users\johan\source\repos\GDanielJ\Weekly Challanges Tim Corey\2. Text File challenge\TextFileChallenge\StandardDataSet.csv";
+
+        BindingList<UserModel> users = new BindingList<UserModel>();
+        FileService fileService = new FileService();
+
+        public ChallengeForm()
+        {
+            InitializeComponent();
+
+            WireUpDropDown();
+        }
+
+        private void WireUpDropDown()
+        {
+            users = fileService.ReadUsers(path);
+            usersListBox.DataSource = users;
+            usersListBox.DisplayMember = nameof(UserModel.DisplayText);
+        }
+
+        private void usersListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addUserButton_Click(object sender, EventArgs e)
+        {
+            users.Add(new UserModel 
+            { 
+                FirstName = firstNameText.Text,
+                LastName = lastNameText.Text,
+                Age = (int)agePicker.Value,
+                IsAlive = isAliveCheckbox.Checked
+            });
+        }
+
+        private void saveListButton_Click(object sender, EventArgs e)
+        {
+            fileService.SaveUsers(users, path);
+        }
+    }
+}
---
+++ 2. Text File challenge/TextFileChallenge/FileService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileChallenge
+{
+    public class FileService
+    {
+
+        public BindingList<UserModel> ReadUsers(string path)
+        {
+            BindingList<UserModel> users = new BindingList<UserModel>();
+
+            using (var reader = new StreamReader(path))
+            {
+                reader.ReadLine();
+
+                while (!reader.EndOfStream)
+                {
+                    var user = new UserModel();
+
+                    var line = reader.ReadLine();
+                    string[] values = line.Split(',');
+
+                    user.FirstName = values[0];
+                    user.LastName = values[1];
+                    user.Age = int.Parse(values[2]);
+                    user.IsAlive = Convert.ToBoolean(Convert.ToInt32(values[3]));
+
+                    users.Add(user);
+                }
+
+                return users;
+            }
+        }
+
+        public void SaveUsers(BindingList<UserModel> users, string path)
+        {
+            using(var writer = new StreamWriter(path, false))
+            {
+                foreach (var user in users)
+                {
+                    int isAliveInt = user.IsAlive == true ? 1 : 0;
+
+                    string line = $"{user.FirstName},{user.LastName},{user.Age},{isAliveInt}";
+
+                    writer.WriteLine(line);
+                }
+            }
+        }
+    }
+}
