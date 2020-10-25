bf115d5265dfeaf95104ed45504d5a57ce6bcc9c
Daniel Johansson <johansson.gd@gmail.com>
2020-10-25 13:01:39 +0100

    klar med main challenge

---
+++ 3. Error Handling challenge/ConsoleUI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentProcessor paymentProcessor = new PaymentProcessor();
+            for (int i = 0; i <= 10; i++)
+            {
+                try
+                {
+                    var result = paymentProcessor.MakePayment($"Demo{ i }", i);
+
+                    if (result == null)
+                    {
+                        Console.WriteLine($"Null value for item {i}");
+                    }
+                    else
+                    {
+                        Console.WriteLine(result.TransactionAmount);
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Payment skipped for payment with {i} items");
+                }
+
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
