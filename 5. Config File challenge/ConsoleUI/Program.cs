bb7341cbc663b8d55ebf36f801e120050c88b5fb
Daniel Johansson <johansson.gd@gmail.com>
2020-11-15 12:32:14 +0100

    Klar med main challenge

---
+++ 5. Config File challenge/ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+            string appSetting = ConfigurationManager.AppSettings["TempFilePath"];
+
+            Console.WriteLine(connectionString);
+            Console.WriteLine(appSetting);
+
+            Console.ReadLine();
+        }
+    }
+}
