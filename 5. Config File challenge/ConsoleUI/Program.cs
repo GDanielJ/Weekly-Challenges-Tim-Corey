0a03df6bc0b7594f0174b7f79ae0b25db8100de4
Daniel Johansson <johansson.gd@gmail.com>
2020-11-15 14:09:02 +0100

    Klar med bonus challenge

--- 5. Config File challenge/ConsoleUI/Program.cs
+++ 5. Config File challenge/ConsoleUI/Program.cs
@@ -12,11 +12,51 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // Main challenge //
             string connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
             string appSetting = ConfigurationManager.AppSettings["TempFilePath"];
 
             Console.WriteLine(connectionString);
             Console.WriteLine(appSetting);
+
+            Console.ReadLine();
+
+            // Bonus challenge //
+
+            var connectionStrings = ConfigurationManager.ConnectionStrings;
+
+            foreach (ConnectionStringSettings cS in connectionStrings)
+            {
+                Console.WriteLine($"Name: {cS.Name}, Value: {cS.ConnectionString}");
+            }
+
+            Console.ReadLine();
+
+            var appSettings = ConfigurationManager.AppSettings;
+
+            foreach (var key in appSettings.AllKeys)
+            {
+                Console.WriteLine($"Key: {key}, Value: {appSettings[key]}");
+            }
+
+            Console.ReadLine();
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            string newKey = "NewKey";
+            string newValue = "NewValue";
+
+            config.AppSettings.Settings.Add(newKey, newValue);
+            config.Save(ConfigurationSaveMode.Modified);
+
+            ConfigurationManager.RefreshSection("appSettings");
+
+            appSettings = ConfigurationManager.AppSettings;
+
+            foreach (var key in appSettings.AllKeys)
+            {
+                Console.WriteLine($"Key: {key}, Value: {appSettings[key]}");
+            }
 
             Console.ReadLine();
         }
