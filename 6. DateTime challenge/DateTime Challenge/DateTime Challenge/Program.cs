a138250f3706a75acc7ca83da32ed97ff0a1f86a
Daniel Johansson <johansson.gd@gmail.com>
2020-11-21 16:53:17 +0100

    klar med Bonus challenge

--- 6. DateTime challenge/DateTime Challenge/DateTime Challenge/Program.cs
+++ 6. DateTime challenge/DateTime Challenge/DateTime Challenge/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace DateTime_Challenge
 {
@@ -6,10 +7,30 @@ namespace DateTime_Challenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Give me a date: ");
+            Console.WriteLine("Give me a date on format dd/MM/yy or MM/dd/yy: ");
             var dateInput = Console.ReadLine();
 
-            var daysAgo = (DateTime.Now - DateTime.Parse(dateInput)).TotalDays;
+            Console.WriteLine("If you used format dd/MM/yy enter 1 and if you used format MM/dd/yy enter 2.");
+
+            string inputFormat;
+            double daysAgo;
+
+            do
+            {
+                inputFormat = Console.ReadLine();
+
+                if (int.Parse(inputFormat) == 1)
+                {
+                    daysAgo = (DateTime.Now - DateTime.ParseExact(dateInput, "dd/MM/yy", CultureInfo.InvariantCulture)).TotalDays;
+                    break;
+                }
+                if (int.Parse(inputFormat) == 2)
+                {
+                    daysAgo = (DateTime.Now - DateTime.ParseExact(dateInput, "MM/dd/yy", CultureInfo.InvariantCulture)).TotalDays;
+                    break;
+                }
+                Console.WriteLine("Please enter 1 or 2");
+            } while (true);
 
             Console.WriteLine($"It has been {Math.Round(daysAgo, 2)} days ago since {dateInput}");
 
@@ -17,6 +38,12 @@ namespace DateTime_Challenge
             var timeInput = Console.ReadLine();
 
             var timeAgo = DateTime.Now.Subtract(DateTime.Parse(timeInput));
+
+            if (timeAgo.Ticks < 0)
+            {
+                timeAgo = timeAgo.Add(TimeSpan.FromHours(24));
+            }
+
 
             Console.WriteLine($"{timeInput} was {timeAgo.Hours} hours and {timeAgo.Minutes} minutes ago");
 
