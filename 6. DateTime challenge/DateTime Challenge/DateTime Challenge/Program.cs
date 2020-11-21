be3fd801c83783d9e60af5b0d58dab4c03f9a957
Daniel Johansson <johansson.gd@gmail.com>
2020-11-21 15:49:36 +0100

    Klar med standard challenge

---
+++ 6. DateTime challenge/DateTime Challenge/DateTime Challenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DateTime_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a date: ");
+            var dateInput = Console.ReadLine();
+
+            var daysAgo = (DateTime.Now - DateTime.Parse(dateInput)).TotalDays;
+
+            Console.WriteLine($"It has been {Math.Round(daysAgo, 2)} days ago since {dateInput}");
+
+            Console.WriteLine("Give me a time: ");
+            var timeInput = Console.ReadLine();
+
+            var timeAgo = DateTime.Now.Subtract(DateTime.Parse(timeInput));
+
+            Console.WriteLine($"{timeInput} was {timeAgo.Hours} hours and {timeAgo.Minutes} minutes ago");
+
+            Console.ReadLine();
+        }
+    }
+}
